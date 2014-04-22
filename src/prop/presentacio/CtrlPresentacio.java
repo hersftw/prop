677304aea90392ed1d679b1895fd85efa6e6a3e7
Edgarest <edgarlorenzo17@gmail.com>
2014-04-22 23:03:23 +0200

    ProvaIncident

--- src/prop/presentacio/CtrlPresentacio.java
+++ src/prop/presentacio/CtrlPresentacio.java
@@ -5,12 +5,10 @@ public class CtrlPresentacio {
 		System.out.println("adeu");
 		System.out.println("hola");
 	}
-<<<<<<< HEAD:src/prop/CtrlPresentacio.java
 	
-}
-=======
+
+
 	public void edgar(){
 		hola();
 	}
 }
->>>>>>> 9045d80548c0e45d0ec492081b6a942a948b8aae:src/prop/presentacio/CtrlPresentacio.java
