b2b74d10472ffee638adb78f568ea93edf81448d
mrcllbr <marcllibre@gmail.com>
2014-05-02 10:55:34 +0200

    nodo fet i conjuntonodo creat

---
+++ src/Nodo.java
@@ -0,0 +1,25 @@
+
+public class Nodo {
+	private int x;
+	private int y;
+	
+	public Nodo(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
