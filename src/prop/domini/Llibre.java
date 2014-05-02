8e52aecf7c690169e64aa20f1cdf39296d3c9d0f
RicardPastor <rcrd.pastor@gmail.com>
2014-05-02 13:01:48 +0200

    Revert "Llibre"
    This reverts commit c0765f5df254a9560afbc8ef8d0f4208119d2085.

--- src/prop/domini/Llibre.java
+++ src/prop/domini/Llibre.java
@@ -1,23 +1,7 @@
 package prop.domini;
 
-public class Llibre extends Elemento {
-	private String titol;
+public class Llibre {
+	private int isbn;
 	private String autor;
-	private String editorial;
-	private int any;
-	
-	
-	public Llibre(String titol, String autor, String editorial,
-			int any) {
-		this.titol = titol;
-		this.autor = autor;
-		this.editorial = editorial;
-		this.any = any;
-	}
-	
-	
-	
-	
 	
 }
- 
