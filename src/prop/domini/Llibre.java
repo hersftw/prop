c0765f5df254a9560afbc8ef8d0f4208119d2085
RicardPastor <rcrd.pastor@gmail.com>
2014-05-02 12:31:48 +0200

    Llibre

--- src/prop/domini/Llibre.java
+++ src/prop/domini/Llibre.java
@@ -1,7 +1,23 @@
 package prop.domini;
 
-public class Llibre {
-	private int isbn;
+public class Llibre extends Elemento {
+	private String titol;
 	private String autor;
+	private String editorial;
+	private int any;
+	
+	
+	public Llibre(String titol, String autor, String editorial,
+			int any) {
+		this.titol = titol;
+		this.autor = autor;
+		this.editorial = editorial;
+		this.any = any;
+	}
+	
+	
+	
+	
 	
 }
+ 
