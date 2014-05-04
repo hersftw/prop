f4199e9c71cb3ce55a863e50be895f8477bfee9b
RicardPastor <rcrd.pastor@gmail.com>
2014-05-04 20:03:57 +0200

    Llibre i categoria comentats amb LaTeX, crec

--- src/prop/domini/Categoria.java
+++ src/prop/domini/Categoria.java
@@ -2,15 +2,34 @@ package prop.domini;
 
 public class Categoria {
 	private String nom;
+	
+	
+    /**Constructora por defecte
+    *
+    * @param nom Indica el nom de la categoria.
+    */
 
 	public Categoria(String nom) {
 		this.nom = nom;
 	}
 
+	
+	 /**Consultora del paràmetre nom.
+    *
+    * @return el valor que pren nom.
+
+    */
+	
 	public String getNom() {
 		return nom;
 	}
 
+	
+	/**Modificadora del parametre nom de la categoria.
+    *
+    * @param nom Indica el nou nom de la categoria.
+    */
+	
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
--- src/prop/domini/Llibre.java
+++ src/prop/domini/Llibre.java
@@ -10,7 +10,17 @@ public class Llibre extends Elemento {
 	private Categoria categoria;
 	
 	
-	public Llibre(int id, String autor, String titol, String editorial,
+    /**Constructora por defecte
+    *
+    * @param id Identificador del llibre, no pot haver repetits. 
+    * @param titol Indica el nom del llibre.
+    * @param autor Indica el nom del autor del llibre.
+    * @param editorial Indica la editorial que va publicar el llibre.
+    * @param any Indica el any en que es va publicar el llibre.
+    * @param categoria Indica la categoria temàtica a la que pertany el llibre.
+    */
+	
+	public Llibre(int id, String titol, String autor,  String editorial,
 			int any, Categoria categoria) {
 		super(id);
 		this.autor = autor;
@@ -20,62 +30,128 @@ public class Llibre extends Elemento {
 		this.categoria = categoria; 
 	}
 
+	
+	 /**Consultora del paràmetre id.
+    *
+    * @return el valor que pren id.
 
+    */
+	
 	public int getId() {
 		return id;
 	}
 
 
+	/**Modificadora del parametre id del llibre
+    *
+    * @param id Indica el nou id del llibre.
+    */
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 
+		 /**Consultora del paràmetre titol.
+    *
+    * @return el valor que pren titol. 
+
+    */
+	
+	public String getTitol() {
+		return titol;
+	}
+	
+	
+	/**Modificadora del parametre titol del llibre
+    *
+    * @param titol Indica el nou títol del llibre.
+    */
+
+	public void setTitol(String titol) {
+		this.titol = titol;
+	}
+	
+	
+	 /**Consultora del paràmetre Autor.
+    *
+    * @return el valor que pren autor.
+
+    */
 
 	public String getAutor() {
 		return autor;
 	}
 
 
+	/**Modificadora del parametre Autor del llibre
+    *
+    * @param autor Indica el nou autor del llibre.
+    */
+	
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
 
 
-	public String getTitol() {
-		return titol;
-	}
+	 /**Consultora del paràmetre editorial.
+    *
+    * @return el valor que pren editorial.
 
-
-	public void setTitol(String titol) {
-		this.titol = titol;
-	}
-
-
+    */
+	
 	public String getEditorial() {
 		return editorial;
 	}
 
-
+	
+	/**Modificadora del parametre editorial del llibre
+    *
+    * @param editorial Indica la nova editorial del llibre.
+    */
+	
 	public void setEditorial(String editorial) {
 		this.editorial = editorial;
 	}
 
+	
+	 /**Consultora del paràmetre any.
+    *
+    * @return el valor que pren any.
 
+    */
+	
 	public int getAny() {
 		return any;
 	}
 
 
+	/**Modificadora del parametre any del llibre
+    *
+    * @param any Indica el nou any de publicació del llibre.
+    */
+	
 	public void setAny(int any) {
 		this.any = any;
 	}
 
+	
+	 /**Consultora del paràmetre categoria.
+    *
+    * @return el valor que pren categoria.
 
+    */
+	
 	public Categoria getCategoria() {
 		return categoria;
 	}
 
 
+	/**Modificadora del parametre id del llibre
+    *
+    * @param categoria Indica la nova categoria del llibre.
+    */
+	
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
 	}
