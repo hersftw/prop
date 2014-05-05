a34dc146906cf1be6618b15b65363dce0a8d1ad3
Edgarest <edgarlorenzo17@gmail.com>
2014-05-05 21:02:19 +0200

    comentats --> Distribucio/llibreria/conica/lineal

--- src/prop/domini/Conica.java
+++ src/prop/domini/Conica.java
@@ -2,7 +2,11 @@ package prop.domini;
 
 public class Conica extends Llibreria {
 	
-	/**Constructora por defecto**/
+	/**Constructora por defecto
+    *
+    * @param prestatges Número de prestatges de la llibreria cònica. 
+    * @param ubicacions  Número d'ubicacions de la llibreria cònica.
+    */
 	public Conica(int prestatges, int ubicacions){
 		super(prestatges, ubicacions);
 	}
--- src/prop/domini/Lineal.java
+++ src/prop/domini/Lineal.java
@@ -1,7 +1,11 @@
 package prop.domini;
 
 public class Lineal extends Llibreria {
-	/**Constructora por defecto**/
+	/**Constructora por defecto
+    *
+    * @param prestatges Número de prestatges de la llibreria lineal. 
+    * @param ubicacions  Número d'ubicacions de la llibreria lineal.
+    */
 	public Lineal(int prestatges, int ubicacions){
 		super(prestatges, ubicacions);
 	}
