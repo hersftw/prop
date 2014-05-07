6396b1f6294c68c5ed44fc27e81843e338d5e0b1
mrcllbr <marcllibre@gmail.com>
2014-05-07 16:10:29 +0200

    controlador llibres i driver

--- src/prop/ctrldomini/CrtlLlibres.java
+++ src/prop/ctrldomini/CrtlLlibres.java
@@ -6,4 +6,20 @@ import prop.domini.Llibre;
 
 public class CrtlLlibres {
 	private List<Llibre> llibres;
+	
+	public void consultar_llibre(){
+		
+	}
+	
+	public void afegir_llibre(){
+		
+	}
+	
+	public void modificar_llibre(){
+		
+	}
+	
+	public void eliminar_llibre(){
+		
+	}
 }
