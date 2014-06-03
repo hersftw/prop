c623565581d32bf1061c5ab630614de060408e6d
RicardPastor <rcrd.pastor@gmail.com>
2014-06-03 12:39:01 +0200

    Afegir llibre captura dades fet

--- src/prop/presentacio/vistaAfegirLlibre.java
+++ src/prop/presentacio/vistaAfegirLlibre.java
@@ -23,12 +23,12 @@ public class vistaAfegirLlibre extends JFrame {
 	private JTextField txtEditorial;
 	private JTextField txtAny;
 	private JTextField txtCategoria;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField teditorial;
+	private JTextField tISBN;
+	private JTextField ttitol;
+	private JTextField tautor;
+	private JTextField tany;
+	private JTextField tcategoria;
 	
 	
 	
@@ -122,43 +122,51 @@ public class vistaAfegirLlibre extends JFrame {
 		contentPane.add(txtCategoria);
 		txtCategoria.setColumns(10);
 		
-		textField = new JTextField();
-		textField.setBounds(204, 204, 259, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		teditorial = new JTextField();
+		teditorial.setBounds(204, 204, 259, 20);
+		contentPane.add(teditorial);
+		teditorial.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(204, 111, 259, 20);
-		contentPane.add(textField_1);
-		textField.addActionListener(new ActionListener(){
-			public void actionPerformed (ActionEvent e) {
-				String titol = textField.getText();
-			}
-		});
+		tISBN = new JTextField();
+		tISBN.setColumns(10);
+		tISBN.setBounds(204, 111, 259, 20);
+		contentPane.add(tISBN);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(204, 142, 259, 20);
-		contentPane.add(textField_2);
+		ttitol = new JTextField();
+		ttitol.setColumns(10);
+		ttitol.setBounds(204, 142, 259, 20);
+		contentPane.add(ttitol);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(204, 173, 259, 20);
-		contentPane.add(textField_3);
+		tautor = new JTextField();
+		tautor.setColumns(10);
+		tautor.setBounds(204, 173, 259, 20);
+		contentPane.add(tautor);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(204, 235, 259, 20);
-		contentPane.add(textField_4);
+		tany = new JTextField();
+		tany.setColumns(10);
+		tany.setBounds(204, 235, 259, 20);
+		contentPane.add(tany);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(204, 265, 259, 20);
-		contentPane.add(textField_5);
+		tcategoria = new JTextField();
+		tcategoria.setColumns(10);
+		tcategoria.setBounds(204, 265, 259, 20);
+		contentPane.add(tcategoria);
 		
 		
 		JButton button_1 = new JButton("");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				int isbn = Integer.parseInt(tISBN.getText());
+				String titol = ttitol.getText();
+				String autor = tautor.getText();
+				String editorial = teditorial.getText();
+				int any = Integer.parseInt(tany.getText());
+				String categoria = tcategoria.getText();
+				
+
+				
+			}
+		});
 		button_1.setIcon(new ImageIcon(vistaGestionarEstructura.class.getResource("/prop/icons/tick.png")));
 		button_1.setBounds(559, 416, 65, 35);
 		contentPane.add(button_1);
